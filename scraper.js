e3ff85885d42182cfb2db9da218d9cc1981a181f
mountainfirefly <pankajdcoder@gmail.com>
2021-08-23 10:27:38 +0530

    Update scraper

--- scraper.js
+++ scraper.js
@@ -6,34 +6,32 @@ const fieldOrder = [
   'name',
   'rank',
   'description',
-  'founders',
   'sector',
-  'jobCreated',
-  'batch'
+  'jobs-created',
+  'batch',
+  'ycg-batch',
+  'headquarters',
+  'careers',
 ]
 
 request({ url: 'https://www.ycombinator.com/topcompanies', gzip: true }, function (error, response, html) {
   if (!error && response.statusCode == 200) {
     const $ = cheerio.load(html);
     const data = [];
-    $('#main-companies').children('tr').each(function(i, element){
+    $('.companies-table').children('tbody').children('tr').each(function(i, element){
       const startup = {};
       $(this).children('td').each(function(i, element) {
         const currentField = fieldOrder[i];
 
         if (currentField === 'name') {
-          startup.name = $(this).find('b').text();
-          const url = $(this).find('a').attr('href');
+          startup.name = $(this).find('.company-name').text();
+          const url = $(this).find('.company-name').attr('href');
           if (url) {
-            startup.key = url.split('.')[0].substring(2);
+            startup.key = url.split('.')[0].substring(1);
           }
-        } else if (currentField === 'founders') {
-          const founders = [];
-          $(this).find('li').each(function(index, e) {
-            founders.push($(this).text());
-          })
-
-          startup[currentField] = founders;
+        } else if (currentField === 'careers') {
+          const url = $(this).find('a').attr('href');
+          startup[currentField] = url;
         } else {
           startup[currentField] = $(this).text();
         }
